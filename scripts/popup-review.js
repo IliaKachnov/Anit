8fffff803c35dd2d9e3ee0902cfb899462d1961a
IliaKachnov <123602127+IliaKachnov@users.noreply.github.com>
2023-09-26 07:59:02 +0200

    Add files via upload

--- scripts/popup-review.js
+++ scripts/popup-review.js
@@ -2,8 +2,6 @@ const openReviewModals = document.querySelectorAll('#popup__review');
 const closeNameModals = document.querySelectorAll('.popup__image-wrapper');
 const modalReview = document.querySelector('#popup__review-body');
 
-
-
 openReviewModals.forEach((openReviewModal) => {
   openReviewModal.addEventListener('click', () => {
     modalReview.classList.add('open');
@@ -17,12 +15,9 @@ closeNameModals.forEach((closeNameModal) => {
     document.body.style.overflow = 'visible';
   });  
 })
-
-
 const letterImg = document.querySelector('.letter__img');
 const imgFullscreenWrapper = document.querySelector('.img__fullscreen-wrapper');
 const crestFull = document.querySelector('.crest__full');
-
 
 letterImg.addEventListener('click', () => {
   imgFullscreenWrapper.classList.remove('hide');
